6e7dd6858b6e5813b064bc7aa4bbf24e35f5d7c2
Evandro oliveira <vandoaparecido@hotmail.com>
2021-03-12 01:31:52 -0300

    Criando navegação com Drawer navigation

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import Routes from './src/routers';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
   );
 }
 
