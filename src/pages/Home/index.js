25eb65cb39e3fea57764d4f1b4ff8e10da80daa4
Evandro oliveira <vandoaparecido@hotmail.com>
2021-03-12 02:39:14 -0300

    Criando header e condições do clima

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import {SafeAreaView,StyleSheet, Text}from 'react-native';
 
-import Menu from '../../component/Menu'
+import Menu from '../../component/Menu';
+import Header from '../../component/Header';
+import Conditions from '../../component/Conditions';
 
 export default function Home (){
     return(
         <SafeAreaView style={styles.container}>
             <Menu />
+            <Header />
+            <Conditions />
         </SafeAreaView>
     );
 }
