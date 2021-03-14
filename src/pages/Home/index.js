a038fc85de0b0e209c245acbf49aaba34ae838a1
Evandro oliveira <vandoaparecido@hotmail.com>
2021-03-14 01:43:11 -0300

    Tirando barra de rolagem no iphone

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -83,6 +83,7 @@ export default function Home (){
             <Conditions weather={weather} />
 
             <FlatList 
+                showsHorizontalScrollIndicator={false}
                 horizontal={true}
                 contentContainerStyle={{paddingBottom:'5%'}}
                 style={styles.list}
